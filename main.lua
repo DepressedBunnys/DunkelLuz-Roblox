730e6bf23b20db02d73f7f284aba24d3c30fbd92
Bunnys <78443317+DepressedBunnys@users.noreply.github.com>
2022-11-15 00:43:58 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,13 @@
-print('Hello')
+--wait(3) -- Timeout function
+game.Workspace.Part.BrickColor = BrickColor.new("Black") -- Navigating to the brick
+
+--wait(4)
+-- game.Workspace.Part2.BrickColor = BrickColor.new("Maroon") -- Navigating to the second brick
+
+game.Workspace.Part.Part2.BrickColor = BrickColor.new("Maroon") -- Navigating to the child of brick one which is brick 2
+
+-- Changing position & anchoring the box
+
+game.Workspace.Part.Anchored = true
+
+game.Workspace.Part.Position = Vector3.new(5, 10, 2)
